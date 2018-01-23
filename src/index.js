46b462b6d7580a8eb6961ef4969bf9bf868882b9
Anton <its.for.regs@gmail.com>
2018-01-23 14:22:16 +0400

    add brain-even

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import readlineSync from 'readline-sync';
 
 const greeting = () => {
   const name = readlineSync.question('May I have your name? ');
-  return `Hello, ${name}!`;
+  console.log(`Hello, ${name}!\n`);
+  return name;
 };
 
 export default greeting;
