02dc9228253f00a4b87ad02532496ff40c669297
Anton <its.for.regs@gmail.com>
2018-01-24 16:30:56 +0400

    remove debug output

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,7 +6,6 @@ const gameTask = 'What is the result of the expression?\n';
 const getQuestion = () => `${getRandomNumber()} ${getRandomMath()} ${getRandomNumber()}`;
 const getAnswer = (str) => {
   const math = str.split(' ');
-  console.log(math);
   if (math[1] === '+') {
     return +math[0] + +math[2];
   } else if (math[1] === '-') {
