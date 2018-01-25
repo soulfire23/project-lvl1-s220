1f2325551041bf7852d4d311dc80d781bf5d40e4
Anton <its.for.regs@gmail.com>
2018-01-25 21:29:03 +0400

    refactor brain-balance

--- src/games/balance.js
+++ src/games/balance.js
@@ -3,23 +3,24 @@ import { getRandomNumber } from '../utils';
 
 const rules = 'Balance the given number.';
 
+const isBalanced = numArray => !((numArray[0] + 1) < numArray[numArray.length - 1]);
+
+const getBalance = (num) => {
+  const numArray = num.split('').sort();
+  for (let i = 0; i < numArray.length; i += 1) {
+    numArray[i] = Number(numArray[i]);
+  }
+  if (isBalanced(numArray)) {
+    return numArray.join('');
+  }
+  numArray[0] += 1;
+  numArray[numArray.length - 1] -= 1;
+  return getBalance(numArray.join(''));
+};
+
 const gameTask = () => {
   const question = getRandomNumber(100, 1000).toString();
-  const getBalance = (num) => {
-    const numArray = num.split('').sort();
-    for (let i = 0; i < numArray.length; i += 1) {
-      numArray[i] = Number(numArray[i]);
-    }
-    if ((numArray[0] + 1) < numArray[numArray.length - 1]) {
-      numArray[0] += 1;
-      numArray[numArray.length - 1] -= 1;
-      return getBalance(numArray.join(''));
-    }
-    return numArray.join('');
-  };
-
   const answer = getBalance(question);
-
   return [question, answer];
 };
 
